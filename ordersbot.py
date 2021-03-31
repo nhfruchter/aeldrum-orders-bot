8b61fe090e6bbdd5c2071e01d349c73fcbddf805
BruceDanielFeldman <brucedanielfeldman@outlook.com>
2021-03-30 23:05:43 -0400

    Allow multiple orders
    Extends the bot to facilitate multiple orders in a message

--- ordersbot.py
+++ ordersbot.py
@@ -113,87 +113,84 @@ async def on_message(message):
 
           except:
             await message.channel.send("Turns must be an integer > 0.")
-            await message.add_reaction("❌")     
+            await message.add_reaction("❌") 
+
+    async def order(order_content):
+     files = await files_from_attachments(message.attachments)
+
+     # Get turn, if present
+     order_turn = (
+       USER_SPECIFIED_TURNS.get(requestor.id) or 
+       USER_SPECIFIED_TURNS.get(DEFAULT_USER) or ""
+     )
+      
+     # Affiliation/title for embed: either from role or specified in brackets
+     # !sendorder foo => role, !sendorder[something else] foo => "something else"
+     affiliation = requestor.top_role
+     content = order_content
+
+     # Add ' a' as hack to avoid empty splits (e.g. for input string '')
+     maybe_affiliation = (order_content + ' a').split()[0]
+     if (maybe_affiliation.startswith('[') and maybe_affiliation.endswith(']')):
+       affiliation = maybe_affiliation.replace("[", "").replace("]", "")
+       content = content[len(maybe_affiliation):].strip()
+
+     # Create fancy Discord embed
+     title = "Order from %s" % affiliation
+     if order_turn:
+       title = "*[Turn %s]* " % order_turn + title
+
+     out_message = discord.Embed(title=title, description=content)
+
+     # Set embed author to sender
+     out_message.set_author(
+       name=requestor.display_name, icon_url=requestor.avatar_url
+     )
+
+     logging.debug(
+       "%s, %s, %s, files: %s",
+       requestor.display_name,
+       affiliation,
+       content,
+       len(files),
+     )
+     
+     await order_channel.send(embed=out_message, files=files)
+     await message.add_reaction("👍")
+     
+     if LOG_TO_SHEET:
+       # Currently: Timestamp, Turn, Faction, Author, Content,	Attachments
+       update_row = [
+         datetime.datetime.now().strftime("%x %X"),
+         order_turn,
+         str(affiliation),
+         requestor.display_name,
+         content,
+         " ".join([a.url for a in message.attachments]) or ""
+       ]
+       logging.debug("Sent update to sheet %s" % update_row)
+       sheetslogger.update(LOG_TO_SHEET, update_row)
+      
 
     async def _orders():
-        parsed = re.findall(CMD_ORDER, message.content, re.IGNORECASE + re.MULTILINE)
-
-        if len(parsed):
-            # args[0] = blank if no alt affiliation
-            args = parsed[0]
-            # Handle multiline messages by removing the command from the message content instead of 
-            # relying on regex output
-            content = re.sub(r"^" + CMD_PREFIX + r"(\[[^\]]*])?", "", message.content).strip()
-            
-            # Grab the files, if any
-            files = await files_from_attachments(message.attachments)
-
-            # Affiliation/title for embed: either from role or specified in brackets
-            # !sendorder foo => role, !sendorder[something else] foo => "something else"
-            affiliation = (
-                args[0].replace("[", "").replace("]", "")
-                if args[0]
-                else requestor.top_role
-            )
-
-            # Get turn, if present
-            order_turn = (
-              USER_SPECIFIED_TURNS.get(requestor.id) or 
-              USER_SPECIFIED_TURNS.get(DEFAULT_USER) or ""
-            )
-
-            # Create fancy Discord embed
-            title = "Order from %s" % affiliation
-            if order_turn:
-                title = "*[Turn %s]* " % order_turn + title
-
-            out_message = discord.Embed(title=title, description=content)
-
-            # Set embed author to sender
-            out_message.set_author(
-                name=requestor.display_name, icon_url=requestor.avatar_url
-            )
-
-            logging.debug(
-                "%s, %s, %s, files: %s",
-                requestor.display_name,
-                affiliation,
-                content,
-                len(files),
-            )
-
-            await order_channel.send(embed=out_message, files=files)
-            await message.add_reaction("👍")
-            
-            if LOG_TO_SHEET:
-                # Currently: Timestamp, Turn, Faction, Author, Content,	Attachments
-                update_row = [
-                    datetime.datetime.now().strftime("%x %X"),
-                    order_turn,
-                    str(affiliation),
-                    requestor.display_name,
-                    content,
-                    " ".join([a.url for a in message.attachments]) or ""
-                ]
-                logging.debug("Sent update to sheet %s" % update_row)
-                sheetslogger.update(LOG_TO_SHEET, update_row)
-                
-    # Register bot commands through the patterns needed to match their flags            
-    CMD_ORDER = r"^" + CMD_PREFIX + r"(\[[^\]]*])?((?:\s+?\n+?)?.+)?"
-    CMD_TURN = r"^" + TURN_PREFIX
-    CMD_HELP = r"^" + HELP_PREFIX
+      # Match command prefix using word boundary.
+      # Note: This misses edge cases (like !!sendorder)... pray for sanity
+      CMD_PATTERN = r'%s\b' % CMD_PREFIX
+    
+      # Ignore leading '' caused by matching !sendorder prefix
+      for order_content in re.split(CMD_PATTERN, message.content)[1:]:
+        await order(order_content.strip())
 
     # ...and the functions that parse the command
     commands = {
-        CMD_ORDER: _orders,
-        CMD_TURN: _turn,
-        CMD_HELP: _help,
+        CMD_PREFIX: _orders,
+        TURN_PREFIX: _turn,
+        HELP_PREFIX: _help,
     }
 
     for matcher, fn in commands.items():
-        is_match = re.findall(matcher, message.content, re.IGNORECASE + re.MULTILINE)
-        if len(is_match):
-            return await fn()
+      if (message.content.startswith(matcher)):
+        return await fn()
 
     # If user sends invalid command to bot, send help text.
     if message.channel.type == discord.ChannelType.private:
@@ -215,7 +212,6 @@ if __name__ == "__main__":
     # Configurable stuff
     # Google Sheet ID or False if you want it off
     LOG_TO_SHEET = "1RHM0_hfx2pOs6_TrWzCwcN6QLBlSPl42ul6E_UWbR0Y"
-    # LOG_TO_SHEET = False
     
     CMD_PREFIX = "!sendorder"
     TURN_PREFIX = "!turn"
